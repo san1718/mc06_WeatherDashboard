69c894fd603da998a02c7ea1f14c0c3c574d8ec8
Sung Min An <an.sungmin17@icloud.com>
2025-01-04 14:13:13 -0800

    key moved

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,3 @@
-// API Key: 6bdabfafea6c9fb1c11b7b85ca98c4ca
-
 // GIVEN a weather dashboard with form inputs
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
